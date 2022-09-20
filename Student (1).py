5d1499df0c8ecadee0699f7fcffc914d53c09c57
KunduKingshuk <110808767+KunduKingshuk@users.noreply.github.com>
2022-09-20 11:20:15 +0530

    Add files via upload

---
+++ Student (1).py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+pd.read_csv('Students.tsv',sep="\t")
+
+
+# In[5]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/DSData/master/Advertising.csv")
+
+
+# In[9]:
+
+
+pd.read_csv("Advertising.csv")
+
+
+# In[ ]:
+
+
+
+
